d51b33a2aee0cb20a4d94a984639ece78d180029
Rummy99-398681 <ny3500xx@gmail.com>
2024-04-25 22:53:50 +0900

    スタイル追加

--- lib/color.dart
+++ lib/color.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 abstract class MyColor {
   static const background = Color(0xFF15212B);
   static const foreground = Color(0xFFFFFFFF);
+  static final foregroundSub = const Color(0xFFFFFFFF).withOpacity(0.6);
 }
--- lib/style.dart
+++ lib/style.dart
@@ -6,17 +6,63 @@ class MyStyle {
   static const _enFont = GoogleFonts.sarpanch;
   static const _jpFont = GoogleFonts.mPlus1p;
 
+  // テキスト系 (英字)
   static final title = _enFont(
     color: MyColor.foreground,
     fontSize: 20,
     fontWeight: FontWeight.bold,
   );
 
+  static final text = _enFont(
+    color: MyColor.foreground,
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final subText = _enFont(
+    color: MyColor.foreground,
+    fontSize: 11,
+    fontWeight: FontWeight.bold,
+  );
+
+  // テキスト系 (日本語)
+  static final titleJp = _jpFont(
+    color: MyColor.foreground,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final name = _jpFont(
+    color: MyColor.foreground,
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final textJp = _jpFont(
+    color: MyColor.foreground,
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final subTextJp = _jpFont(
+    color: MyColor.foregroundSub,
+    fontSize: 10,
+    fontWeight: FontWeight.bold,
+  );
+
+  // コンポーネント系
   static final button = _enFont(
     color: MyColor.foreground,
     fontSize: 14,
     fontWeight: FontWeight.bold,
-    letterSpacing: 1.25,
+    letterSpacing: 1.1,
+  );
+
+  static final buttonJp = _jpFont(
+    color: MyColor.foreground,
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1.1,
   );
 
   /// テキストフィールドのキャプション(英字)
