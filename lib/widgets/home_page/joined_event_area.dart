6dd02a07dcddec65bd4b74350a489657e6bb4e4b
Rummy99-398681 <ny3500xx@gmail.com>
2024-05-03 16:23:00 +0900

    エリア分け＆ウィジェット化

---
+++ lib/widgets/home_page/joined_event_area.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class JoinedEventArea extends StatelessWidget {
+  const JoinedEventArea({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 352,
+      height: double.infinity,
+      color: Colors.green,
+    );
+  }
+}
