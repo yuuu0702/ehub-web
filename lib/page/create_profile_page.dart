7cc2763a6edef2cf679afc73cd6d2fd025f55ab1
yudai <yudai.0702wa@gmail.com>
2024-06-06 23:00:05 +0900

    テキストフォームを可変に変更、プラットフォームボタン作成

--- lib/page/create_profile_page.dart
+++ lib/page/create_profile_page.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:ehub_web/style.dart';
 import 'package:ehub_web/widgets/my_filled_button.dart';
 import 'package:ehub_web/widgets/my_text_form_field.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,10 @@ class CreateProfilePage extends StatefulWidget {
 
 class _CreateProfilePageState extends State<CreateProfilePage> {
   Uint8List? _userImage;
+  var platformPc = false;
+  var platformPlayStation = false;
+  var platformSwitch = false;
+  var platformMobile = false;
 
   @override
   Widget build(BuildContext context) {
@@ -31,109 +36,162 @@ class _CreateProfilePageState extends State<CreateProfilePage> {
 
     return Scaffold(
       backgroundColor: const Color(0xFF15212B),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 130,
-              child: Image.asset('assets/images/ehub_logo.png'),
-            ),
-            const SizedBox(height: 36),
-            Row(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: SizedBox(
+            width: 600,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Column(
+                SizedBox(
+                  height: 130,
+                  child: Image.asset('assets/images/ehub_logo.png'),
+                ),
+                const SizedBox(height: 36),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    _userImage == null
-                        ? const CircleAvatar(
-                            radius: 50,
-                            backgroundImage: NetworkImage(
-                              'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg',
-                            ),
-                          )
-                        : CircleAvatar(
-                            radius: 50,
-                            backgroundImage: MemoryImage(_userImage!),
+                    SizedBox(
+                      width: 120,
+                      child: Column(
+                        children: [
+                          _userImage == null
+                              ? const CircleAvatar(
+                                  radius: 50,
+                                  backgroundImage: NetworkImage(
+                                    'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg',
+                                  ),
+                                )
+                              : CircleAvatar(
+                                  radius: 50,
+                                  backgroundImage: MemoryImage(_userImage!),
+                                ),
+                          ElevatedButton(
+                            onPressed: pickImage,
+                            child: const Text('画像を選択'),
                           ),
-                    ElevatedButton(
-                      onPressed: pickImage,
-                      child: const Text('画像を選択'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 36),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          MyTextFormField(
+                            labelText: 'Name',
+                            onChanged: (String value) {
+                              email = value;
+                            },
+                          ),
+                          const SizedBox(height: 36),
+                          SizedBox(
+                            width: 1000,
+                            child: MyTextFormField(
+                              labelText: '所属部署',
+                              onChanged: (String value) {
+                                email = value;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-                const SizedBox(width: 36),
+                const SizedBox(height: 36),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(
-                      width: 320,
-                      child: MyTextFormField(
-                        labelText: 'Name',
-                        onChanged: (String value) {
-                          email = value;
-                        },
-                      ),
+                    Text(
+                      'プラットフォーム',
+                      style: MyStyle.text,
                     ),
-                    const SizedBox(height: 36),
-                    SizedBox(
-                      width: 320,
-                      child: MyTextFormField(
-                        labelText: '所属部署',
-                        onChanged: (String value) {
-                          email = value;
-                        },
-                      ),
+                    Wrap(
+                      direction: Axis.horizontal,
+                      spacing: 40,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              platformPc = !platformPc;
+                            });
+                          },
+                          child: FaIcon(
+                            FontAwesomeIcons.computer,
+                            color: platformPc
+                                ? Colors.white
+                                : const Color(0xFF2C3740),
+                            size: 48,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              platformPlayStation = !platformPlayStation;
+                            });
+                          },
+                          child: FaIcon(
+                            FontAwesomeIcons.playstation,
+                            color: platformPlayStation
+                                ? Colors.white
+                                : const Color(0xFF2C3740),
+                            size: 48,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              platformSwitch = !platformSwitch;
+                            });
+                          },
+                          child: SvgPicture.asset(
+                            'svgs/nintendo_switch_icon_136357.svg',
+                            height: 48,
+                            width: 48,
+                            color: platformSwitch
+                                ? Colors.white
+                                : const Color(0xFF2C3740),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              platformMobile = !platformMobile;
+                            });
+                          },
+                          child: FaIcon(
+                            FontAwesomeIcons.mobile,
+                            color: platformMobile
+                                ? Colors.white
+                                : const Color(0xFF2C3740),
+                            size: 48,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
+                const SizedBox(height: 36),
+                SizedBox(
+                  width: 320,
+                  child: MyTextFormField(
+                    maxLines: 3,
+                    labelText: '紹介文',
+                    onChanged: (String value) {
+                      password = value;
+                    },
+                  ),
+                ),
+                const SizedBox(height: 24),
+                MyFilledButton(
+                  text: 'Login',
+                  width: 160,
+                  onTap: () {},
+                ),
               ],
             ),
-            const SizedBox(height: 36),
-            Row(
-              children: [
-                FaIcon(
-                  FontAwesomeIcons.computer,
-                  size: 48,
-                ),
-                FaIcon(
-                  FontAwesomeIcons.playstation,
-                  size: 48,
-                ),
-                SvgPicture.asset(
-                  'svgs/nintendo_switch_icon_136357.svg',
-                  height: 48,
-                  width: 48,
-                )
-              ],
-            ),
-            SizedBox(
-              width: 320,
-              child: MyTextFormField(
-                labelText: 'Name',
-                onChanged: (String value) {
-                  email = value;
-                },
-              ),
-            ),
-            const SizedBox(height: 36),
-            SizedBox(
-              width: 320,
-              child: MyTextFormField(
-                labelText: '所属部署',
-                onChanged: (String value) {
-                  password = value;
-                },
-              ),
-            ),
-            const SizedBox(height: 24),
-            MyFilledButton(
-              text: 'Login',
-              width: 160,
-              onTap: () {},
-            ),
-          ],
+          ),
         ),
       ),
     );
